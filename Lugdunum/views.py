d29c49d3cacaed0bdd94f5381bdeab5474dd5785
Nathan Casals <nathan.casals@smartprospective.com>
2018-04-25 09:13:04 +0200

    added download view

--- Lugdunum/views.py
+++ Lugdunum/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
 from django.utils.encoding import smart_str
-
+from django.conf import settings
+from django.http import Http404
+import os
+def download(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
 def index(request):
-    response = HttpResponse(content_type='application/force-download') # mimetype is replaced by content_type for django 1.7
-    response['Content-Disposition'] = 'attachment; filename=%s' % smart_str("zbra.jpg")
-    response['X-Sendfile'] = smart_str("media/")
+    
+    response = HttpResponse('<p>Hey</p>') # mimetype is replaced by content_type for django 1.7
     # It's usually a good idea to set the 'Content-Length' header too.
     # You can also set any other required headers: Cache-Control, etc.
     return response
